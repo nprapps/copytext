d80f850e5755d2802e0e3a2e39026591836161a6
Christopher Groskopf <staringmonkey@gmail.com>
2014-05-21 15:23:46 -0700

    Rev version. Update CHANGELOG.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='copytext',
-    version='0.1.1',
+    version='0.1.2',
     description='A library for accessing a spreadsheet as a native Python object suitable for templating.',
     long_description=open('README').read(),
     author='NPR Visuals Team',
