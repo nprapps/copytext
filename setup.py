e8a0b8b8b86d9d3977b730a88d3e834d02a36cb6
Christopher Groskopf <staringmonkey@gmail.com>
2015-01-27 10:02:23 -0600

    Rev to 0.1.8 for development.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='copytext',
-    version='0.1.7',
+    version='0.1.8',
     description='A library for accessing a spreadsheet as a native Python object suitable for templating.',
     long_description=open('README').read(),
     author='NPR Visuals Team',
